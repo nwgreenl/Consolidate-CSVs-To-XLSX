6bb7c01cf75b41ddcf7ce72ca7df106122f2e6e3
Nick Greenlees <greenleesnick@gmail.com>
2020-06-27 13:57:51 -0600

    Cleaning up per conventions & adding progressbar

--- main.py
+++ main.py
@@ -1,38 +1,78 @@
 import datetime, os, re, sys, pandas as pd
 from tkinter import filedialog
 
-# get files via tkinter
+# cwd
 cwd = os.getcwd()
-files = filedialog.askopenfilenames(initialdir=cwd, title="Select File", filetypes=[("CSV Files", ".csv")])
-if len(files) == 0:
-    sys.exit("\nNo Files Selected... Goodbye!\n")
 
-# output dir
-outputDirName = "output"
-if not os.path.exists("%s/%s" % (cwd, outputDirName)):
-    os.mkdir("%s/%s" % (cwd, outputDirName))
+# get files via tkinter filedialog
+def get_files():
+    files = filedialog.askopenfilenames(initialdir=cwd, title="Select File", filetypes=[("CSV Files", ".csv")])
+    
+    if len(files) == 0:
+        sys.exit("\nNo Files Selected... Goodbye!\n")
+    
+    return list(files)
+
+# output dir (assumes it's in root folder)
+def get_outputdir(dirName="output"):
+    output_dir = "%s/%s" % (cwd, dirName)
+    
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    
+    return output_dir
 
 # output file 
-outputDate = datetime.datetime.now().strftime("%m-%d-%Y_%I-%M") 
-outputFileName = "consolidated_%s" % outputDate
-outputExt = "xlsx"
-outputFile = "%s/%s/%s.%s" % (cwd, outputDirName, outputFileName, outputExt)
+def get_outputfile(fileName="consolidated", dirName="output"):
+    output_ext = "xlsx"
+    output_dir = get_outputdir(dirName)
+    output_date = datetime.datetime.now().strftime("%m-%d-%Y_%I-%M") 
 
-# sheet name regex
-illegalChars = re.compile("[^a-zA-Z0-9]")
+    output_file_name = "%s_%s" % (fileName, output_date)
+    output_file = "%s/%s.%s" % (output_dir, output_file_name, output_ext)
+    
+    return output_file
+
+# progress bar, thanks @eusoubrasileiro
+def progressbar(it, prefix="Running: ", size=50, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()        
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
 
 # consolidate
-print("Running...\n")
-with pd.ExcelWriter(outputFile, engine="xlsxwriter") as writer:  
-    for file in files:
-        # sheet names must be <= 31 chars and cannot contain "\ / * ? : ,"
-        # opting to remove any char that isn't a word or digit
-        fileNameForSheet = illegalChars.sub("", os.path.basename(file)).replace("csv", "")[:31]
-        
-        df = pd.read_csv(file)
-        df.to_excel(writer, sheet_name=fileNameForSheet, index=False)
+def consolidate_files(files, output_file):  
+    # sheet name regex
+    illegal_chars = re.compile("[^a-zA-Z0-9]")
 
-# success message
-isInputPlural = "s" if len(files) > 1 else ""
-print("Successfully created '%s' by consolidating the following CSV%s:\n  - " % (outputFileName, isInputPlural), end="")
-print(*files, sep = "\n  - ")
+    try:        
+        with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
+            for i in progressbar(range(len(files))): 
+                for file in files:
+                    # sheet names must be <= 31 chars and cannot contain "\ / * ? : ,"
+                    # opting to remove any char that isn't a word or digit
+                    file_name_for_sheet = illegal_chars.sub("", os.path.basename(file)).replace("csv", "")[:31]
+                    
+                    df = pd.read_csv(file)
+                    df.to_excel(writer, sheet_name=file_name_for_sheet, header=False, index=False)
+
+        # success message
+        is_input_plural = "s" if len(files) > 1 else ""
+
+        print("\nSuccessfully created '%s' using the following CSV%s:" % (os.path.basename(output_file), is_input_plural))
+        for file in files:
+            print("  - %s" % os.path.basename(file))    
+
+    except Exception as e:
+        print("Error encountered while consolidating files")
+        print(e)
+
+# do the things
+consolidate_files(get_files(), get_outputfile())
