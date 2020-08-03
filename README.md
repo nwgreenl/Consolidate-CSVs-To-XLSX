13beaad3f4b9aa0b0842f2c5fae3f06fe5166d7b
Nick Greenlees <greenleesnick@gmail.com>
2020-08-02 18:45:02 -0600

    Updating strings and README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Consolidate CSVs to XLSX
-Simple python script that consolidates multiple CSVs into a single Excel file (with multiple sheets)
+Simple python script that consolidates multiple CSVs into a single Excel file (with multiple sheets).
 
 **Example**
 
@@ -12,7 +12,8 @@ Simple python script that consolidates multiple CSVs into a single Excel file (w
 - xlsxwriter (`pip3 install xlsxwriter`)
 
 ### Usage
-Run `main.py` and select the CSVs you wish to consolidate via the file dialog. 
+- Run `main.py` and select the CSVs you wish to consolidate via the file dialog. 
+- Assumes first row is the header.
 
 ### Customization
 Output directory & file name via the final call to `get_outputfile()`
@@ -21,4 +22,4 @@ Output directory & file name via the final call to `get_outputfile()`
 
 Header styling via `header_format`. The spec can be found [here](https://xlsxwriterlua.readthedocs.io/working_with_formats.html)
 
-- Defaults are set to be center alignment (vertical and horizontal), no borders, bold text, & text wrapping
+- Defaults are set to be center alignment (vertical and horizontal), no borders, bold text, & text wrapping.
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_files():
 
 # output dir (assumes it's in root folder)
 def get_outputdir(dir_name="output"):
-    output_dir = "%s/%s" % (cwd, dir_name)
+    output_dir = f"{cwd}/{dir_name}"
     
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -32,11 +32,11 @@ def get_outputfile(filename="consolidated", dir_name="output", append_date=True)
     output_date = datetime.datetime.now().strftime("%m-%d-%Y_%I-%M") 
 
     if append_date:
-        output_filename = "%s_%s" % (filename, output_date)
+        output_filename = f"{filename}_{output_date}"
     else:
-        output_filename = "%s_%s" % filename
+        output_filename = filename
 
-    output_file = "%s/%s.%s" % (output_dir, output_filename, output_ext)
+    output_file = f"{output_dir}/{output_filename}.{output_ext}"
     
     return output_file
 
@@ -45,8 +45,8 @@ def progressbar(it, prefix="Running: ", size=50, file=sys.stdout):
     count = len(it)
     def show(j):
         x = int(size*j/count)
-        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
-        file.flush()        
+        file.write(f"{prefix}[{'#' * x}{'.' * (size - x)}] {j}/{count}\r")
+        file.flush()
     show(0)
     for i, item in enumerate(it):
         yield item
@@ -83,9 +83,9 @@ def consolidate_files(files, output_file):
         # success message
         is_input_plural = "s" if len(files) > 1 else ""
 
-        print("\nSuccessfully created '%s' at %s using the following CSV%s:" % (os.path.basename(output_file), os.path.dirname(output_file), is_input_plural))
+        print(f"\nSuccessfully created '{os.path.basename(output_file)}' at {os.path.dirname(output_file)} using the following CSV{is_input_plural}:")
         for file in files:
-            print("  - %s" % os.path.basename(file))    
+            print(f"  - {os.path.basename(file)}")    
 
     except Exception as e:
         print("Error encountered while consolidating files")
